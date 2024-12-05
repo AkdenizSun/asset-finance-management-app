e781afc1b762b788771cc44305b80c99f8bfae18
Yana <syanaa9@gmail.com>
2024-12-05 17:17:27 +1100

    Resister and login pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,31 +1,59 @@
-import React from 'react'
-import {Form, Input} from 'antd';
-import { Link } from "react-router-dom";
+import React, {useState} from 'react'
+import {Form, Input, message} from 'antd';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import Spinner from '../components/Layout/Spinner';
 
 const Login = () => {
-        //from submit
-        const submitHandler= (values) => {
-            console.log(values);
+    const [loading, setLoading] = useState(false); 
+    const navigate = useNavigate();
+
+        const submitHandler= async (values) => {
+            try {
+                setLoading(true);
+                const {data} = await axios.post('/api/v1/users/login',values);
+                setLoading(false);
+                message.success('login success');
+                localStorage.setItem('user', JSON.stringify({...data, password:''}));
+                navigate('/')
+            } catch (error) {
+                setLoading(false);
+                message.error(error.response?.data?.message || 'Login failed');
+            }
         };
   return (
     <>
-    <div className="register-page">
-        <Form layout='vertical' onFinish={submitHandler}>
-            <h1>Login Form</h1>
-            <Form.Item label="Email" name="email">
-                <Input type="email"/>
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-                <Input type="password"/>
-            </Form.Item>
-            <div className='d-flex justify-content-between'>
-                <Link to="/register">Not a user ? Click Here to register</Link>
-                <button className='btn btn-primary'>Login</button>
-            </div>
+      <div className="register-page">
+        {loading && <Spinner />}
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Login Form</h1>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input type="password" />
+          </Form.Item>
+
+          <div className="d-flex justify-content-between">
+            <Link to="/register">Not a user ? Click Here to register</Link>
+            <button className="btn btn-primary">Login</button>
+          </div>
         </Form>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Login
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,34 +1,73 @@
-import React from 'react';
-import {Form, Input} from 'antd';
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
+import {Form, Input, message} from 'antd';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import Spinner from '../components/Layout/Spinner';
 
 const Register = () => {
+const navigate = useNavigate();
+const [loading, setLoading] = useState(false);
     //from submit
-    const submitHandler= (values) => {
+    const submitHandler = async (values) => {
         console.log(values);
+        try {
+            setLoading(true);
+            await axios.post('/api/v1/users/register', values);
+            message.success("Registration was successful");
+            setLoading(false);
+            navigate('/login');
+        } catch (error) {
+            setLoading(false);
+            message.error('invalid username or password');
+        }
     };
   return (
     <>
-        <div className="register-page">
-        <Form layout='vertical' onFinish={submitHandler}>
-            <h1>Register Form</h1>
-            <Form.Item label="Name" name="name">
-                <Input />
-            </Form.Item>
-            <Form.Item label="Email" name="email">
-                <Input type="email" />
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-                <Input type="password" />
-            </Form.Item>
-            <div className='d-flex justify-content-between'>
-                <Link to="/login">Already Register ? Click Here to Login</Link>
-                <button className='btn btn-primary'>Login</button>
-            </div>
+      <div className="register-page">
+        {loading && <Spinner />}
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Register Form</h1>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: "Please input your password!" },
+              {
+                min: 6,
+                message: "Password must be at least 6 characters long!",
+              },
+            ]}
+          >
+            <Input type="password" />
+          </Form.Item>
+          
+          <div className="d-flex justify-content-between">
+            <Link to="/login">Already Register ? Click Here to Login</Link>
+            <button className="btn btn-primary">Register</button>
+          </div>
         </Form>
-        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Register
